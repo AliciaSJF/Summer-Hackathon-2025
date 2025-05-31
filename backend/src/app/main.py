df0a5e9e9921ee50fb8028f93eec595965ad7a73
daniel.exposito@habber.com <daniel.exposito@habber.com>
2025-05-31 13:28:15 +0200

    feat: cors

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -6,8 +6,18 @@ from src.app.routers.event import router as event_router
 from src.app.routers.reservation import router as reservation_router
 from src.app.routers.user import router as user_router
 from src.app.routers.recommendation import router as recommendation_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Reputation Platform API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(business_router)
 app.include_router(event_router)
